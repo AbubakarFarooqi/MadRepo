374ea9923de8a8b4230b1c2883c10c4fadbb7ded
Abubakar Farooqi <101521634+AbubakarFarooqi@users.noreply.github.com>
2023-11-06 14:49:55 +0500

    Add ch 11 assignment

---
+++ ch_11_assignemnt/android/app/src/main/kotlin/com/example/ch_11_assignemnt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ch_11_assignemnt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ch_11_assignemnt/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:ch_11_assignemnt/Task2.dart';
+import 'package:ch_11_assignemnt/Task3.dart';
+import 'package:ch_11_assignemnt/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    // home: MyHomePage(),
+    // home: Task2(),
+    home: Task3(),
+  ));
+}
